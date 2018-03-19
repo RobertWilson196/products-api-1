340b4d18a43516fa794ef59c59c5259e11bca39f
Robert Wilson <youmufan@gmail.com>
2018-03-19 19:36:23 -0400

    extracted our logger and placed it into a new folder called middleware, then imported it back into the index.js

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const serverApp = express();
 const mongoose = require('mongoose');
 
 require('dotenv').config();
+
+//middleware
+const logger = require('./middleware/logger');
+
 mongoose.connect(process.env.MONGO_URI);
 
 const PORT = process.env.PORT || 5000; //necessary for Heroku deployment
@@ -10,13 +14,7 @@ const PORT = process.env.PORT || 5000; //necessary for Heroku deployment
 //routers
 const productRouter = require('./routers/products');
 
-serverApp.use(function logger(req, res, next){
-    const { url } = req;
-    const date = new Date();
-    console.log(`URL: ${url}, ${date}`)
-    next();
-});
-
+serverApp.use(logger);
 serverApp.use(productRouter); //register the router with the application
 
 serverApp.get('/', (req, res) => {
