3aa12d28dfa620a834d4e9e2d8ea9bcfbc71efc1
Robert Wilson <youmufan@gmail.com>
2018-03-19 20:22:29 -0400

    extracted serverError handler into middleware folder and reimported into index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require('dotenv').config();
 //middleware
 const logger = require('./middleware/logger'); //morgan is better
 const err404 = require('./middleware/404');
+const serverError = require('./middleware/serverError');
 
 mongoose.connect(process.env.MONGO_URI);
 
@@ -23,11 +24,7 @@ serverApp.get('/', (req, res) => {
 });
 
 serverApp.use(err404); //express docs to find out how to implement a custom url page
-serverApp.use(function serverErrorHandler(err, req, res, next){
-    res.status(500).json({
-        msg: "something went wrong"
-    });
-});
+serverApp.use(serverError);
 
 serverApp.listen(PORT, () => {
     console.log(`Now listening on port ${PORT}`);
